ab9b57eccd54fa2ee3b5119e8fa17fee9eddf019
CelesteHrdez <l25580513@reynosa.tecnm.mx>
2025-10-14 11:51:54 -0600

    Practica 1
    Crea un Gift

---
+++ Preactica 1, gift animado.cs
@@ -0,0 +1,20 @@
+namespace Programacion_practica_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Stretchlmage(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Amarillo(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
